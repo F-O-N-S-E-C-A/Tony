becdabeca00bff4508daa8e5591bc9fb8c48074f
F-O-N-S-E-C-A <tiago_fonseca@iscte-iul.pt>
2022-03-29 15:30:17 +0100

    QR code reader and generator

--- QR_reader.py
+++ QR_reader.py
@@ -1,6 +1,5 @@
 import cv2
 
-
 def draw(img, points):
     for i in points:
         img_draw = cv2.rectangle(img, (int(i[0][0]), int(i[0][1])), (int(i[2][0]), int(i[2][1])), (0, 255, 0), 3)
@@ -15,5 +14,5 @@ retval, decoded_info, points, straight_qrcode = det.detectAndDecodeMulti(img)
 
 print('position: ', points)
 
-draw(img, points)0
+draw(img, points)
 print('uuid: ', decoded_info)
